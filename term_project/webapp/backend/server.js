ba3e63d11133a7a99f575f8586b945dfd5ec0ac4
Chris Guarino <chrisguarino@chriss-mbp.myfiosgateway.com>
2024-12-05 15:00:12 -0500

    trying to make live app open access

--- term_project/webapp/backend/server.js
+++ term_project/webapp/backend/server.js
@@ -34,6 +34,6 @@ app.use("/api/sales", salesRoutes);
 
 // Start the server
 const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
